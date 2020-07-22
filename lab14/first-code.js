d68965e07ce1ea0137713ccab2c2f414a490042d
sukhmeetsandhar <66041026+sukhmeetsandhar@users.noreply.github.com>
2020-07-22 16:32:17 -0700

    .

--- lab14/first-code.js
+++ lab14/first-code.js
@@ -1,8 +1,38 @@
-say_hello= function(){
-count = 54;
-count = count * 2;
-length = 7.32;
-hello = "hello";
-msg = hello;
-alert(msg + 'World' + count);
+add = function() {
+result = parseInt(jQuery('#operand_1').html()) + parseInt(jQuery('#operand_2').html());
+jQuery('#result').html(result);
 }
+setup = function() {
+jQuery('#add_button').click(add)
+}
+jQuery(document).ready(setup)
+
+
+sub = function() {
+result = parseInt(jQuery('#operand_1').html()) - parseInt(jQuery('#operand_2').html());
+jQuery('#result').html(result);
+}
+setup = function() {
+jQuery('#sub_button').click(sub)
+}
+jQuery(document).ready(setup)
+
+
+mul = function() {
+result = parseInt(jQuery('#operand_1').html()) * parseInt(jQuery('#operand_2').html());
+jQuery('#result').html(result);
+}
+setup = function() {
+jQuery('#mul_button').click(mul)
+}
+jQuery(document).ready(setup)
+
+
+div = function() {
+result = parseInt(jQuery('#operand_1').html()) / parseInt(jQuery('#operand_2').html());
+jQuery('#result').html(result);
+}
+setup = function() {
+jQuery('#div_button').click(div)
+}
+jQuery(document).ready(setup)
---
+++ lab14a/code.js
@@ -0,0 +1,9 @@
+say_hello= function(){
+count = 54;
+count = count * 2;
+length = 7.32;
+hello = "hello";
+msg = hello;
+space = '';
+alert("Hello World!");
+}
